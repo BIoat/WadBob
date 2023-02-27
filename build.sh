785598d1e1e29a702bee95f98439afd11f9bb48e
sleep <49450642+BIoat@users.noreply.github.com>
2023-02-28 00:37:59 +0100

    filesize

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Linux
-GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin
+GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o out/bin
+
 
 # Windows
-CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o bin.exe
+CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o out/bin.exe
 
 # Finish
-strip bin bin.exe && mv bin bin.exe out/
-cp out/bin.exe /home/anon/wadbot/
+cd out && strip bin bin.exe && upx --lzma bin bin.exe && sync
+# lzma -qv9T 8 bin bin.exe
 
+
+cp bin.exe /home/anon/wadbot/
 # fyne-cross windows -arch=amd64
